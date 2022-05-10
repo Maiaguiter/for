50258ae1341c15d91e522ff81a6e356f744f0164
Maiaguiter <49686906@est.ort.edu.ar>
2022-05-10 08:59:56 -0300

    Initial commit

---
+++ Assets/cubo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubo : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject clon;
+    public int NumeroCubos;
+    public int Gradocubos;
+
+    public void CloneObject()
+    {
+        for(int i=0; i < NumeroCubos; i++)
+        {
+            clon = Instantiate(prefab);
+            clon.transform.Rotate(0, Gradocubos * i, 0);
+            clon.transform.Translate(i, i, 0);
+            //Destroy(clon,.5f);
+        }
+    }
+    }
+
